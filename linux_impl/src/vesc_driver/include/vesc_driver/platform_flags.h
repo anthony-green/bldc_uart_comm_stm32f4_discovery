f572cccaf304292c507e407da1609b9e673ca9ed
anthony-green <anthony.green@piaggiofastforward.com>
2017-09-15 14:31:27 -0400

    add compile error if trying to compile for M4

--- linux_impl/src/vesc_driver/include/vesc_driver/platform_flags.h
+++ linux_impl/src/vesc_driver/include/vesc_driver/platform_flags.h
@@ -3,4 +3,8 @@
 
 #define PLATFORM_IS_LINUX (!defined(TM4C123GH6PM) && !defined(TM4C123GH6PZ) && !defined(TM4C123GH6PGE))
 
+#if !(PLATFORM_IS_LINUX)
+#error Make sure you're prepared to handle this...
+#endif
+
 #endif // PLAT_FLAGS_H_
